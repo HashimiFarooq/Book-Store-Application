4410609e5848ed2eb8cddd93d801f1a2d46c6be4
unknown <kaichengxu12@hotmail.com>
2022-03-29 17:39:14 -0400

    added book class

---
+++ src/coe528project/Book.java
@@ -0,0 +1,27 @@
+package coe528project;
+
+public class Book {
+    private String name;
+    private double price;
+    
+    public Book(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}

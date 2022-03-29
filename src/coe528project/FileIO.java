66f4ef5ced44e1ff5917a40fae6057f4a2dbd44e
unknown <kaichengxu12@hotmail.com>
2022-03-29 18:13:03 -0400

    forgot to save FileIO earlier

--- src/coe528project/FileIO.java
+++ src/coe528project/FileIO.java
@@ -1,7 +1,29 @@
 package coe528project;
 
 import java.util.ArrayList;
+import java.io.*;
 
 public final class FileIO {
+    private static String bookFile = "./books.txt";
+    
     public static ArrayList<Book> books = new ArrayList<Book>();
+    
+    public static void saveBooks(){
+        try {
+            FileWriter fileWriter = new FileWriter(bookFile, true);
+            for (int i = 0; i < books.size(); i++){
+                Book book = books.get(i);
+                fileWriter.write(book.getName() + "\n");
+                fileWriter.write(book.getPrice() + "\n");
+            }
+            fileWriter.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+    
+    public static void loadBooks(){
+        
+    }
 }
